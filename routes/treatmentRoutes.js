156dd0f4c8928bb8333419bc8701108f1329590e
MATHEUS MANOEL DE SANTANA <MATHEUS8400@IOT>
2024-11-04 10:38:10 -0300

    Add treatment CRUD functionality with routes and controllers
    - Implemented treatment creation, retrieval, update, and deletion.
    - Created routes for accessing treatment data by ID, pet name, and tutor name.
    - Organized treatment-related functions into a dedicated controller module.

--- routes/treatmentRoutes.js
+++ routes/treatmentRoutes.js
@@ -3,19 +3,16 @@ const express = require('express');
 const router = express.Router();
 
 // Importing CRUD functions for treatment
-const { createTreatment } = require('../controllers/treatmentControllers/createTreatment');
-const { readTreatmentById, readTreatmentByPetName, readTreatmentByTutorName, readAllTreatments } = require('../controllers/treatmentControllers/readTreatment');
-const { updateTreatment } = require('../controllers/treatmentControllers/updateTreatment');
-const { deleteTreatment } = require('../controllers/treatmentControllers/deleteTreatment');
+const treatmentControllers = require("../controllers/treatmentControllers/treatmentExports");
 
 // Defining routes for treatment CRUD operations
-router.post('/', createTreatment); // Create a new treatment
-router.get('/id/:treatmentId', readTreatmentById); // Read treatment by ID
-router.get('/pet/:petName', readTreatmentByPetName); // Read treatment by pet's name
-router.get('/tutor/:tutorName', readTreatmentByTutorName); // Read treatment by tutor's name
-router.get('/treatments', readAllTreatments); // Read all treatments
-router.put('/:treatmentId', updateTreatment); // Update an existing treatment
-router.delete('/:treatmentId', deleteTreatment); // Delete a treatment
+router.post('/', treatmentControllers.createTreatment); // Create a new treatment
+router.get('/id/:treatmentId', treatmentControllers.readTreatmentById); // Read treatment by ID
+router.get('/pet/:petName', treatmentControllers.readTreatmentByPetName); // Read treatment by pet's name
+router.get('/tutor/:tutorName', treatmentControllers.readTreatmentByTutorName); // Read treatment by tutor's name
+router.get('/treatments', treatmentControllers.readAllTreatments); // Read all treatments
+router.put('/:treatmentId', treatmentControllers.updateTreatment); // Update an existing treatment
+router.delete('/:treatmentId', treatmentControllers.deleteTreatment); // Delete a treatment
 
 // Exporting the router for use in other parts of the application
 module.exports = router;
